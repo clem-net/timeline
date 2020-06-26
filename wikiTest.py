8a1e689e3cbd29c79837a38ea2e8a28aa6ffda82
Clément Petit <clementineorangeypetit@gmail.com>
2020-06-26 17:11:02 +0100

    Understood how API works

--- wikiTest.py
+++ wikiTest.py
@@ -1,2 +1,14 @@
 import wikipedia
-print(wikipedia.summary("Wikipedia"))
+
+ford = wikipedia.page(pageid=645042)
+# 645042
+# print(ford.summary)
+# print("\n", ford.pageid)
+
+for i in range(1, 999):
+    try:
+        page = wikipedia.page(pageid=i)
+        print(page.title)
+    except wikipedia.exceptions.PageError as e:
+        
+        print(str(e))
