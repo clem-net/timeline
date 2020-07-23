1a92aab2f0ab22b1069ce975c7c2cd039a9799cb
Clément Petit <clementineorangeypetit@gmail.com>
2020-07-23 20:08:59 +0100

    Added validation rules to what pages are saved.
    Pages which are lists and talks are validated as false.

--- getUrls.py
+++ getUrls.py
@@ -2,17 +2,34 @@ import wikipedia
 
 
 def main():
-    for i in range(30, 999):
+    for i in range(0, 999):
         try:
             if i % 10 == 0:
                 continue
             page = wikipedia.page(pageid=i)
-            print(i, " ", page.url)
+            validatePage(page)
+            # print(i, " ", page.url)
         except wikipedia.exceptions.PageError as e:
-            print(str(e))
-            # pass
+            pass
         except wikipedia.exceptions.DisambiguationError as e:
-            print(str(e))
+            pass
+
+
+# Attribute page is a wikipedia page
+def validatePage(page):
+    # Validate unless page is
+    # a talk page
+    # a list
+    split = "/wiki/"
+    urlTitle = page.url.split(split)[1]
+    if urlTitle.startswith("Talk:"):
+        print(urlTitle)
+        return False
+    elif urlTitle.startswith("List"):
+        print(urlTitle)
+        return False
+    else:
+        return true
 
 
 if __name__ == "__main__":
